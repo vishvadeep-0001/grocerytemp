d4ec06c56fa9f3a35ed4acf454a1a769e0f3375b
Vishvadeep <vishvadeep.0001@example.com>
2026-07-26 11:37:29 +0530

    11:37

--- client/src/components/Home/ProductCard.tsx
+++ client/src/components/Home/ProductCard.tsx
@@ -1,15 +1,83 @@
-import type { Product } from "../../types"
+import { Star } from "lucide-react";
+import type { Product } from "../../types";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
-  product:Product
+  product: Product;
 }
 
-const ProductCard = ({product} : Props) => {
-   return (
-    <div>
-      
+const ProductCard = ({ product }: Props) => {
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
+
+  const { addToCart } = { addToCart: (_data: any) => {} };
+
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="bg-white rounded-2xl overflow-hidden shadow hover:shadow-md transition-all duration-300 group animate-fade-in cursor-pointer"
+      onClick={() => navigate(`/products/${product._id}`)}
+    >
+      {/* image */}
+      <div className="relative aspect-square overflow-hidden">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-full h-full object-cover p-4 group-hover:p-2 transition-all duration-300"
+        />
+        {/* badges */}
+        <div className="absolute top-3 left-3 flex flex-wrap gap-1.5">
+          {product.discount > 0 && (
+            <span className="px-2 py-0.5 text-[10px] font-semibold uppercase bg-app-orange text-white rounded-full">
+              {product.discount}% OFF
+            </span>
+          )}
+        </div>
+      </div>
+      {/*  */}
+      <div className="p-3.5 text-zinc-700">
+        <h3 className="text-sm leading-snug mb-1.5 line-clamp-2">
+          {product.name}
+        </h3>
+        {/* rating */}
+        {product.rating > 0 && (
+          <div className="flex items-center gap-1 mb-2">
+            <Star className="size-3 text-app-warning fill-app-warning" />
+            <span className="text-xs font-medium text-app-text">
+              {product.rating}
+            </span>
+            <span className="text-xs text-app-text-light">
+              {product.reviewCount}
+            </span>
+          </div>
+        )}
+
+        {/* Price + Add */}
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-1 truncate">
+            <span className="text-base font-medium ">
+              {currency}
+              {product.price.toFixed(1)}
+            </span>
+            <span className="text-xs text-app-text-light block">
+              {product.unit}
+            </span>
+            {product.originalPrice > product.price && (
+              <span className="text-xs text-app-text-light line-through ml-1.5">
+                {currency}
+                {product.originalPrice.toFixed(1)}
+              </span>
+            )}
+          </div>
+          <button className="">
+            
+          </button>
+        </div>
+      </div>
+
+      {/* info */}
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
