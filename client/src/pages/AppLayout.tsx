382dac95d99b4ea5fae5a0d9825bd58eeb7f7622
Vishvadeep <vishvadeep.0001@example.com>
2026-08-03 23:15:14 +0530

    Home section completed and move to another component "Sidebar".

--- client/src/pages/AppLayout.tsx
+++ client/src/pages/AppLayout.tsx
@@ -1,20 +1,20 @@
-import { Outlet } from "react-router-dom"
-import Banner from "../components/Banner"
-import Navbar from "../components/Navbar"
-
+import { Outlet } from "react-router-dom";
+import Banner from "../components/Banner";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const AppLayout = () => {
   return (
     <>
-  <Banner/>
-    <Navbar/>
-     <main className="min-h-screen">
-      <Outlet/>
-     </main>
-     <p>Footer</p>
-     <p>cart side bar</p>
+      <Banner />
+      <Navbar />
+      <main className="min-h-screen">
+        <Outlet />
+      </main>
+      <Footer />
+      <p>cart side bar</p>
     </>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
